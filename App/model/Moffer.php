599f877972711d6a70ebc52605d830d2bf9e88ec
rara <auroragiuliani13@gmail.com>
2025-05-07 18:52:31 +0200

    set e get automatici

--- App/model/Moffer.php
+++ App/model/Moffer.php
@@ -21,7 +21,7 @@ private stateoffer $state;
 private DateTime $dateofferin;
 
 #[ORM\Column]
-private DateTime $dareofferout;
+private DateTime $dateofferout;
 
 #[ORM\ManyToOne(inversedBy:'offers')]
 #[ORM\JoinColumn(name:'post',referencedColumnName:'id')]
@@ -40,11 +40,6 @@ public function getId()
     return $this->id;
 }
 
-public function setId($id)
-{
-    $this->id = $id;
-}
-
 public function getState()
 {
     return $this->state;
@@ -55,7 +50,31 @@ public function setState($state)
     $this->state = $state;
 }
 
+public function getDateofferin(){
+    return $this->dateofferin;
 }
 
+public function setDateofferin($dateofferin){
+    $this->dateofferin = $dateofferin;
+}
+
+public function getDateofferout(){
+    return $this->dateofferout;
+}
+
+public function setDateofferout($dateofferout){
+    $this->dateofferin = $dateofferout;
+}
+
+public function getPost()
+{
+    return $this->post;
+}
+
+public function setPost($post)
+{
+    $this->post = $post;
+}
+}
 
 ?>
