d29b1be316b3925699366dade51979f0bef405e1
Samuele223 <sadecesaris@gmail.com>
2025-06-29 18:52:08 +0200

    clean

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';   // <--- AGGIUNTA QUESTA RIGA QUI per smarty
-require_once __DIR__ . '/config/autoloader.php'; //i path mannaggia a voi aaaaaaaaaaaa
+require_once __DIR__ . '/vendor/autoload.php';  
+require_once __DIR__ . '/config/autoloader.php'; 
 require_once 'config/doctrine-config.php'; 
-require_once __DIR__ . '/App/controller/CfrontController.php'; // <-- THIS IS NEEDED!
+require_once __DIR__ . '/App/controller/CfrontController.php'; 
 
-//require_once "/install/Installation.php";
-//Installation::install();
-
-
-// Supponiamo che la tua classe si chiami FpersistentManager e abbia un getInstance()
 $persistentManager = FpersistentManager::getInstance();
 
 $fc = new CfrontController();
--- test_smarty.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-if (class_exists('\Smarty\Smarty')) {
-    echo "Smarty caricato correttamente, uau!";
-} else {
-    echo "Smarty NON trovato!";
-}
